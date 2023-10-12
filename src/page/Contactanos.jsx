8a6e2eea3714956191772bac173e4f2f948ba4a1
ArechuaDaniel <daniel.ar98hotmail.com>
2023-10-11 23:20:07 -0500

    contactanos

--- src/page/Contactanos.jsx
+++ src/page/Contactanos.jsx
@@ -1,11 +1,65 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Contactanos = () => {
   return (
     <>
-    <Header/>
-      <h1>contacto</h1>
+      <Header/>
+      <main className='  mx-auto  p-5 md:flex md:justify-center bg-slate-700'>
+        <div className="md:w-2/3 lg:w-2/5 ">
+          <h1 className='text-red-400  text-4xl capitalize mt-36'>Escribenos</h1>
+
+          <form className='my-10 bg-white shadow rounded-lg p-10'>
+          <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='nombre'>Nombre</label>
+              <input
+                type='text'
+                id='nombre'
+                placeholder='Tu nombre'
+                className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+              />
+            </div>
+            
+            <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='email'
+                placeholder='correo@correo.com'
+                className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+              />
+            </div>
+            <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='celular'>Celular</label>
+              <input
+                type='number'
+                id='celular'
+                placeholder='0999999999'
+                className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+              />
+            </div>
+            <div className='my-5'>
+              <label className='uppercase text-gray-600 block text-xl font-bold' htmlFor='mensaje'>Mensaje</label>
+              <textarea
+                
+                id='mensaje'
+                placeholder='Tu mensaje'
+                className='w-full mt-3 p-3 border rounded-xl bg-gray-50'
+              />
+            </div>
+            <input
+              type='submit'
+              value='Enviar'
+              className='bg-sky-700 w-full py-3 text-white mb-5 uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors'
+            />
+          </form>
+        </div>
+
+      </main>
+
+    <Footer/>
+
     </>
   )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
           
         {/* //INSTRUCTORES */}
         <div className='text-white'>
-          <h2 className=' md:text-5xl text-2xl flex justify-center text-red-400 mb-4'>Instructores</h2>
+          <h2 className=' md:text-5xl text-2xl flex justify-center text-red-400 mb-4 uppercase'>Instructores</h2>
           <p className='m-6 text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quis quas ducimus quibusdam vero cum ullam nulla ab est excepturi impedit maxime facere fugiat voluptatibus, odit earum amet commodi illo.lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione et voluptates unde suscipit, cum nihil, nemo ullam voluptatem a nesciunt, incidunt nostrum sint excepturi? Nesciunt, itaque. Officiis cupiditate quia excepturi?</p>
           <div className=' text-xl flex flex-col md:flex-row md:justify-around items-center text-white'>
             <div className='bg-slate-800 w-72 rounded-lg m-6'>
